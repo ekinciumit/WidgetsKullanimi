06b9241dca457801981d469f8efe8dc84d675c8e
ekinciumit <l2011012125@stud.sdu.edu.tr>
2023-12-17 03:08:46 +0300

    0.2.1

--- app/src/main/java/com/example/widgetskullanimi/MainActivity.kt
+++ app/src/main/java/com/example/widgetskullanimi/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.widgetskullanimi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.widgetskullanimi.databinding.ActivityMainBinding
-import kotlin.math.log
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var kontrol = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,23 +25,38 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.buttonResim2.setOnClickListener() {
-            binding.imageView.setImageResource(
-                resources.getIdentifier(
-                    "resim2",
-                    "drawable",
-                    packageName
-                )
-            )
+            binding.imageView.setImageResource(R.drawable.resim2)
         }
 
 
         binding.switch1.setOnCheckedChangeListener { compoundButton, isChecked ->
             if (isChecked) {
-                    Log.e("Widgets","Switch : ON")
+                    Log.e("Sonuc","Switch : ON")
             }
             else{
-                    Log.e( "Widgets","Switch : OFF")
+                    Log.e( "Sonuc","Switch : OFF")
 
+            }
+
+        }
+
+        binding.ToggleButton.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            kontrol = isChecked
+            if (kontrol){
+                val secilenButon =findViewById<Button>(binding.ToggleButton.checkedButtonId)
+                val buttonYazi = secilenButon.text.toString()
+                Log.e("Sonuc" ,buttonYazi)
+            }
+
+        }
+
+
+        binding.buttonGoster.setOnClickListener{
+            Log.e("Sonuc","Switch Durum : ${binding.switch1.isChecked}")
+            if (kontrol){
+                val secilenButon =findViewById<Button>(binding.ToggleButton.checkedButtonId)
+                val buttonYazi = secilenButon.text.toString()
+                Log.e("Sonuc" ,"Toggle Durum : $buttonYazi")
             }
 
         }
